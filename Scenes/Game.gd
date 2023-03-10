3f379b992e3392f1e7459a41315607fe3d344e92
Nikola Whallon <nikola@deepgram.com>
2023-03-10 08:48:57 -0800

    interacting with new tts-enabled server to order pizza

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -4,6 +4,7 @@ var client = WebSocketClient.new()
 
 var phone_number = null
 var code = null
+var connected_to_phone = false
 
 var full_transcript = ""
 
@@ -24,16 +25,21 @@ func _closed(was_clean = false):
 
 func _connected(proto = ""):
 	print("Connected with protocol: ", proto)
+	client.get_peer(1).set_write_mode(WebSocketPeer.WRITE_MODE_TEXT)
 
 func _on_data():
 	var message = client.get_peer(1).get_packet().get_string_from_utf8()
-	
+
 	if phone_number == null:
 		phone_number = message
 		$CanvasLayer/MarginContainer/VBoxContainer/PhoneNumberLabel.text = "PHONE NUMBER: " + message
 	elif code == null:
 		code = message
 		$CanvasLayer/MarginContainer/VBoxContainer/CodeLabel.text = "CODE: " + code
+	elif !connected_to_phone:
+		if message == "connected":
+			connected_to_phone = true
+			client.get_peer(1).put_packet("What kind of pizza do you want? We have cheese, pepperoni, and mushroom.".to_utf8())
 	else:
 		var message_json = JSON.parse(message)
 		if message_json.error == OK:
@@ -45,7 +51,16 @@ func _on_data():
 							full_transcript += " "
 						full_transcript += transcript
 						$CanvasLayer/MarginContainer/VBoxContainer/StreamingTranscriptLabel.text = full_transcript
-					
+
+						if "cheese" in transcript:
+							client.get_peer(1).put_packet("One cheese pizza coming up.".to_utf8())
+							$CanvasLayer/MarginContainer/VBoxContainer/PizzaLabel.text = "PIZZA: CHEESE"
+						if "pepperoni" in transcript:
+							client.get_peer(1).put_packet("One pepperoni pizza coming up.".to_utf8())
+							$CanvasLayer/MarginContainer/VBoxContainer/PizzaLabel.text = "PIZZA: PEPPERONI"
+						if "mushroom" in transcript:
+							client.get_peer(1).put_packet("One mushroom pizza coming up.".to_utf8())
+							$CanvasLayer/MarginContainer/VBoxContainer/PizzaLabel.text = "PIZZA: MUSHROOM"
 
 func _process(_delta):
 	client.poll()
